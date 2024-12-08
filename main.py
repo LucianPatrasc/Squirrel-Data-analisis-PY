286ced652435b5dc7b058ad6b4095a2b8b9ccae4
Lucian Patrasc <lpatrasc@gmail.com>
2024-12-08 08:00:45 +0200

    2 nd commit, numbers of squirrels by color

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-data_dict = data.to_dict()
-print(data_dict)
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+print(grey_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
 
-temp_list = data["temp"].to_list()
-print(temp_list)
 
-print(data["temp"].mean())
